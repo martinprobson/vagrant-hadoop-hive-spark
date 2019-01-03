1daad3fc35226ae8f48e30258a4915e3ba1451c1
daj102 <daj102@gmail.com>
2019-01-03 16:50:17 -0500

    Update setup-ubuntu.sh
    I tried to change the name of the VM in the Vagrantfile, but then got name resolution issues because of the hard-coded reference to node1 here.  Changing it to $HOSTNAME means it always gets the right machine name.

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -5,7 +5,7 @@ source "/vagrant/scripts/common.sh"
 
 function setupHosts {
 	echo "modifying /etc/hosts file"
-        echo "127.0.0.1 node1" >> /etc/nhosts
+        echo "127.0.0.1 $HOSTNAME" >> /etc/nhosts
 	echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" >> /etc/nhosts
 	echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/nhosts
 	cp /etc/nhosts /etc/hosts
